0dc2f89bca4f6a8d470f70aa021481f9ffb95691
YeasinArafatFahad <ayeasin90@gmail.com>
2021-04-20 23:13:01 +0600

    final

---
+++ src/pages/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBoN-OMr06eoquFeY5VVTfEErx7ho2ZZvc",
+  authDomain: "dry-wash-3336b.firebaseapp.com",
+  projectId: "dry-wash-3336b",
+  storageBucket: "dry-wash-3336b.appspot.com",
+  messagingSenderId: "862141405452",
+  appId: "1:862141405452:web:8aa763176f3b29821c1129"
+  };
+  
+export default firebaseConfig;
